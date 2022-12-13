32fb03db6f6446f508f1c14cb0946a9d4b94367b
Matan <mshkalim@armosec.io>
2022-12-13 16:06:12 +0000

    echo component and tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,6 @@ filename=${INPUT_FILENAME}
 tag=${INPUT_TAG}
 component=${INPUT_COMPONENT_NAME}
 
+echo "The new tag for $component wil be: $tag"
+
 patch "$filename" <<< $(diff -U0 -w -b --ignore-blank-lines $filename <(yq eval ".$component.image.tag = \"$tag\"" $filename))
